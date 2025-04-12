872999b42da51266f7b064b7d3a2bbc5d459c2ff
crpier42 <crpier42@gmail.com>
2025-04-12 17:02:39 +0300

    await for auth

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs/server"
 
 export async function PATCH(request: Request) {
     try {
-        const { userId } = auth()
+        const { userId } = await auth()
         if (!userId) {
             return NextResponse.json(
                 { error: "Unauthorized" },
